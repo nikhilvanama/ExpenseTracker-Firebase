45688028bcbd3e93b4e5ea5ac74f1f2d41dcaf07
Nikhil vanama <vanamanikhil15@gmail.com>
2025-06-11 15:26:40 +0530

    removed comment of constants in gitignore file

---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDL069XS5Z3A1RKUPd8dgOu4ah7DQgm248",
+  authDomain: "expense-crud-ang.firebaseapp.com",
+  databaseURL: "https://expense-crud-ang-default-rtdb.firebaseio.com",
+  projectId: "expense-crud-ang",
+  storageBucket: "expense-crud-ang.firebasestorage.app",
+  messagingSenderId: "217972171306",
+  appId: "1:217972171306:web:a778fae1f08eb14aec1953",
+  measurementId: "G-YY20P2K9JT"
+};
