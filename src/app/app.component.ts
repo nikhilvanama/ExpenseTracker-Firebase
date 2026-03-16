12887637c74c211bc22bb4871988bef7d40db514
nikhilvanama <vanamanikhil15@gmail.com>
2026-03-16 10:25:21 +0530

    updated UI using antigravity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var lucide: any;
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,24 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Firebase-crud';
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      setTimeout(() => {
+        if (typeof lucide !== 'undefined') {
+          lucide.createIcons();
+        }
+      }, 100);
+    });
+  }
+
+  ngAfterViewInit() {
+    if (typeof lucide !== 'undefined') {
+      lucide.createIcons();
+    }
+  }
 }
